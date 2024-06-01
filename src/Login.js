c18cfb66c0383163b1cdd3748d966ec1fa67f709
Pratyush271299 <lightyagamikilledl@gmail.com>
2024-06-01 14:39:09 +0530

    second commit

--- src/Login.js
+++ src/Login.js
@@ -19,7 +19,29 @@ const Login = () => {
 
     return (
         <section>
-            
+            <p ref={errRef} className={errMsg ? 'errMsg' : 'offscreen'} aria-live='assertive'>{errMsg}</p>
+            <h1>Sign In</h1>
+            <form>
+                <label htmlFor='username'>Username:</label>
+                <input 
+                    type='text' 
+                    id='username'
+                    ref={userRef}
+                    autoComplete='off'
+                    onChange={e => setUser(e.target.value)}
+                    value={user}
+                    required
+                />
+                <label htmlFor='password'>Password:</label>
+                <input 
+                    type='password' 
+                    id='password'
+                    ref={userRef}
+                    onChange={e => setPwd(e.target.value)}
+                    value={pwd}
+                    required
+                />
+            </form>
         </section>
     )
 }
