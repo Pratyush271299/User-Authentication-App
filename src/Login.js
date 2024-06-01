8ed927af82d66abcc980dce9174e3a51f9e4a1da
Pratyush271299 <lightyagamikilledl@gmail.com>
2024-06-01 14:04:11 +0530

    first commit

---
+++ src/Login.js
@@ -0,0 +1,27 @@
+import {useRef, useState, useEffect } from 'react'
+
+const Login = () => {
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState('');
+    const [pwd, setPwd] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [user, pwd])
+
+    return (
+        <section>
+            
+        </section>
+    )
+}
+
+export default Login;
